b06091f838c89514706241985a97cd8f96fa7e25
tamac <taocom0106@gmail.com>
2024-05-31 17:56:44 +0900

    初期の初期版

---
+++ Assets/Script/GameStart.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    public void start_button()
+    {
+        SceneManager.LoadScene("Game");
+        
+    }
+}
